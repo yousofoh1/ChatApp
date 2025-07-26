000003468d14c3651cd9d29540da9923960b343e
yousofo <jon772002@gmail.com>
2025-07-26 16:38:25 +0300

    latest

--- Domain/Models/Channel.cs
+++ Domain/Models/Channel.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Domain.Models.Shared;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Models
 {
-    public class Channel
+    public class Channel: BaseEntity
     {
+        public string Name { get; set; } = string.Empty;
+        public int ServerId { get; set; }
+        public Server Server { get; set; } = null!;
+        public ICollection<Message> Messages { get; set; } = [];
+
     }
 }
