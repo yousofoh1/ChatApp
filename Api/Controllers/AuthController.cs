12bbd6d457eb8c4eb6d23770b0f1394fba5cd313
yousofo <jon772002@gmail.com>
2025-09-28 16:51:13 +0300

    t

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -2,53 +2,26 @@
 using Core.Dtos.Auth;
 using Core.Dtos.Requests;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Api.Controllers
+namespace Api.Controllers;
+
+public class AuthController(IServicesUOW manager) : BaseController
 {
-    public class AuthController(IServicesUOW manager) : BaseController
+
+    [HttpPost("login")]
+    public async Task<LoginSuccess> Login([FromBody] LoginRequest loginRequest)
     {
-        // GET: api/<AuthController>
-        [HttpGet]
-        public async Task<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<AuthController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<AuthController>
-        [HttpPost("login")]
-        public async Task<LoginSuccess> Login([FromBody] LoginRequest loginRequest)
-        {
-            var loginRDto = await manager.AuthService.LoginAsync(loginRequest);
-            return loginRDto;
-        }
-
-        [HttpPost("register")]
-        public async Task<ActionResult<LoginSuccess>> Register([FromForm] RegisterRequest registerRequest)
-        {
-            var loginRDto = await manager.AuthService.RegisterAsync(registerRequest);
-            return Ok(loginRDto);
-        }
-
-        // PUT api/<AuthController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<AuthController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        var loginRDto = await manager.AuthService.LoginAsync(loginRequest);
+        return loginRDto;
     }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<LoginSuccess>> Register([FromForm] RegisterRequest registerRequest)
+    {
+        var loginRDto = await manager.AuthService.RegisterAsync(registerRequest);
+        return Ok(loginRDto);
+    }
+
 }
--- Core/Services/Common/FilesService.cs
+++ Core/Services/Common/FilesService.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Services.Common
+namespace Core.Services.Common;
+
+public class FilesService
 {
-    internal class FilesService
+    public static async Task<string> SaveFile(IFormFile img, IHostEnvironment env, HttpContext http)
     {
+        var path = Path.Combine(env.ContentRootPath, "wwwroot/assets");
+
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+
+        var fileName = Guid.NewGuid().ToString() + Path.GetExtension(img.FileName);
+        var fullPath = Path.Combine(path, fileName);
+
+        using (var stream = new FileStream(fullPath, FileMode.Create))
+        {
+            await img.CopyToAsync(stream);
+        }
+
+        //full path
+        return http.Request.Host + "/Assets/" + fileName;
     }
 }
